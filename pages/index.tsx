957e3fd132de1212a73c73bc1d6e7d2b6e725955
shahariar50 <shahariar.touhid@gmail.com>
2023-03-06 11:54:02 +0600

    added statistics section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import MainLayout from "components/layout/MainLayout";
 import HeroSection from "components/page/homepage/HeroSection";
 import ServiceSection from "components/page/homepage/ServiceSection";
+import StatisticsSection from "components/page/homepage/StatisticsSection";
 
 export default function Home() {
   return (
     <MainLayout>
       <HeroSection />
       <ServiceSection />
+      <StatisticsSection />
     </MainLayout>
   );
 }
