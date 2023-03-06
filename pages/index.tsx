23bb246f9f32f82fa1ed2e9faf6e3d6b36ab8fdf
shahariar50 <shahariar.touhid@gmail.com>
2023-03-06 11:46:46 +0600

    added service section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import MainLayout from "components/layout/MainLayout";
 import HeroSection from "components/page/homepage/HeroSection";
+import ServiceSection from "components/page/homepage/ServiceSection";
 
 export default function Home() {
   return (
     <MainLayout>
       <HeroSection />
+      <ServiceSection />
     </MainLayout>
   );
 }
