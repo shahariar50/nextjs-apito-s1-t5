b64e28bf79cbc72b459210341db9f4e6dda05929
shahariar50 <shahariar.touhid@gmail.com>
2023-03-12 14:17:27 +0600

    setup sticky navbar

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react";
+
 const Header = () => {
+  useEffect(() => {
+    const navEl = document.querySelector("nav");
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 0) {
+        navEl?.classList.add("not-top");
+      } else {
+        navEl?.classList.remove("not-top");
+      }
+    });
+  }, []);
+
   return (
     <nav
       className="navbar navbar-expand-lg navbar-light fixed-top py-3 d-block"
